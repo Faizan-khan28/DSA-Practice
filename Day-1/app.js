d6d6564997ab6398c76e75dbd3b0d2afd0ab5bb8
faizanKhan <faizankhan46849@gmail.com>
2026-05-02 18:18:16 +0530

    swap two varibales

--- Day-1/app.js
+++ Day-1/app.js
@@ -1,5 +1,14 @@
 //  sum and message
-let a = 10
-let b = 20
+// let a = 10
+// let b = 20
 
-console.log("sum of" + a + "and" + b + "is" + a+b);
+// console.log("sum of" + a + "and" + b + "is" + a+b);
+
+// swap two numbers
+
+let a = 10;
+let b = 20;
+let temp = a;
+a = b;
+b = temp;
+console.log(a,b);
