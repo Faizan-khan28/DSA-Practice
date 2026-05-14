4a3a50d444966d958aa21553083a153d7fe81fe6
faizanKhan <faizankhan46849@gmail.com>
2026-05-14 08:09:41 +0530

    sum of digits ques solved

--- Day-4/app.js
+++ Day-4/app.js
@@ -1,7 +1,18 @@
-let n = 5;
-let i = 1;
+// let n = 5;
+// let i = 1;
 
-while (i<=n) {
-    console.log("hello World");
-    i++
+// while (i<=n) {
+//     console.log("hello World");
+//     i++
+// }
+
+let n = Number(prompt("enter Number"))
+let sum = 0;
+
+while(n>0) {
+    rem = n % 10
+    sum = sum + rem
+    n = Math.floor(n / 10)
 }
+
+console.log(sum);
