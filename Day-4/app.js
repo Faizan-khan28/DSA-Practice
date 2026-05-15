32d560e6c1498176c39f6a3ca81041519a39c218
faizanKhan <faizankhan46849@gmail.com>
2026-05-15 08:59:38 +0530

    automorphic number question solved

--- Day-4/app.js
+++ Day-4/app.js
@@ -17,13 +17,28 @@
 
 // console.log(sum);
 
-let n = Number(prompt("enter Number"))
-let rev = 0;
+// let n = Number(prompt("enter Number"))
+// let rev = 0;
 
+// while(n>0) {
+//   let rem = n % 10
+//   rev = rev * 10 + rem
+//   n = Math.floor(n / 10)
+// }
+
+// console.log(rev)
+
+let n = Number(prompt("enter Number"))
+let copy = n;
+let sq = n * n;
+let count = 0;
 while(n>0) {
-  let rem = n % 10
-  rev = rev * 10 + rem
-  n = Math.floor(n / 10)
+  count ++;
+  n = Math.floor(n/10)
 }
 
-console.log(rev)
+if (sq%Math.pow(10,count) == copy) {
+    console.log("Automorphic number");
+} else {
+     console.log("not Automorphic number");
+}
