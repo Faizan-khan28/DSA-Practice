c402a45998334eab4558b5b47c7c1eb810f3a74e
faizanKhan <faizankhan46849@gmail.com>
2026-05-23 12:29:57 +0530

    repeat hello behafe of user input

--- Day-5/app.js
+++ Day-5/app.js
@@ -1,5 +1,12 @@
-let i = 1
+// let i = 1
+// do {
+//     console.log("hello World");
+//     i++
+// }while(i<=5);
+
+// question Repeat Hello
+let userInput;
 do {
-    console.log("hello World");
-    i++
-}while(i<=5);
+ console.log("hello");
+ userInput= prompt("continue Hello yes / no").toLowerCase()
+}while(userInput === "yes")
