32197f55a960a312c018d8b8a29b954b195f75a5
faizanKhan <faizankhan46849@gmail.com>
2026-05-25 12:53:37 +0530

    guess the number question solved

--- Day-5/app.js
+++ Day-5/app.js
@@ -5,8 +5,26 @@
 // }while(i<=5);
 
 // question Repeat Hello
+// let userInput;
+// do {
+//  console.log("hello");
+//  userInput= prompt("continue Hello yes / no").toLowerCase()
+// }while(userInput === "yes")
+
+//  question quess number
+let com = Math.floor(Math.random() * 100) + 1;
 let userInput;
 do {
- console.log("hello");
- userInput= prompt("continue Hello yes / no").toLowerCase()
-}while(userInput === "yes")
+ userInput = Number(prompt("enter your number b/w 1 to 100"))
+ if(userInput == NaN || userInput<0 || userInput>100) {
+    console.log("enter a valid number")
+    continue;
+ }
+ if(userInput > com) {
+   console.log("Too High");
+ }else if (userInput < com) {
+   console.log("too Low");
+ }else {
+   console.log("congrates num is matched" + com );
+ }
+}while(userInput !== com)
