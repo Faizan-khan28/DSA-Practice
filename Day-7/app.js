57ce0bef18135256c26b3ea5312c6b989b052f12
faizanKhan <faizankhan46849@gmail.com>
2026-06-02 11:26:06 +0530

    print x

--- Day-7/app.js
+++ Day-7/app.js
@@ -10,12 +10,23 @@
 // }
 
 // print tringle
-for(let i = 1; i <= 5; i++) {
-    for(let j = 1; j <= 5-i; j++) {
-        process.stdout.write(" ")
-    }
-     for(let j = 1; j <= i; j++) {
-        process.stdout.write("* ")
+// for(let i = 1; i <= 5; i++) {
+//     for(let j = 1; j <= 5-i; j++) {
+//         process.stdout.write(" ")
+//     }
+//      for(let j = 1; j <= i; j++) {
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+// }
+
+for(let i = 1; i<=5; i++) {
+    for(let j = 1; j<=5; j++) {
+        if(i == j || i+j == 5+1) {
+            process.stdout.write("* ")
+        }else {
+            process.stdout.write(" ")
+        }
     }
     console.log();
 }
