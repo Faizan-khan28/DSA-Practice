9b3c8a07c23bc6a74d3e26c370aa73f30095d0f1
faizanKhan <faizankhan46849@gmail.com>
2026-06-02 07:05:30 +0530

    print tringle done

--- Day-7/app.js
+++ Day-7/app.js
@@ -1,9 +1,21 @@
+// mirror right angle tringle
+// for(let i = 1; i <= 5; i++) {
+//     for(let j = 1; j <= 5-i; j++) {
+//         process.stdout.write("  ")
+//     }
+//      for(let j = 1; j <= i; j++) {
+//         process.stdout.write("* ")
+//     }
+//     console.log();
+// }
+
+// print tringle
 for(let i = 1; i <= 5; i++) {
     for(let j = 1; j <= 5-i; j++) {
         process.stdout.write(" ")
     }
      for(let j = 1; j <= i; j++) {
-        process.stdout.write("*")
+        process.stdout.write("* ")
     }
     console.log();
 }
