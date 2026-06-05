52ee910d2dc43118cf46258826e7e9da0c440412
faizanKhan <faizankhan46849@gmail.com>
2026-06-05 16:49:52 +0530

    print sum of array

--- Day-8/app.js
+++ Day-8/app.js
@@ -1,9 +1,20 @@
 
 // print dynamic array
+// let size = Number(prompt("enter size"))
+// let arr = new Array(size)
+// for(let i =0; i< arr.length; i++) {
+//     arr[i] = prompt("enter element")
+// }
+
+// console.log(arr)
+
+// print sum of array 
 let size = Number(prompt("enter size"))
 let arr = new Array(size)
+let sum = 0;
 for(let i =0; i< arr.length; i++) {
-    arr[i] = prompt("enter element")
+    arr[i] = Number(prompt("enter element"))
+    sum = sum + arr[i];
 }
 
-console.log(arr)
+console.log("sum =" + sum)
