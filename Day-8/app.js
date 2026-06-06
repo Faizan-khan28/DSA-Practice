34cac029cd7b6b423b0dea39cf153a6f5f1dd040
faizanKhan <faizankhan46849@gmail.com>
2026-06-06 13:21:44 +0530

    find max number in array

--- Day-8/app.js
+++ Day-8/app.js
@@ -9,12 +9,23 @@
 // console.log(arr)
 
 // print sum of array 
-let size = Number(prompt("enter size"))
-let arr = new Array(size)
-let sum = 0;
-for(let i =0; i< arr.length; i++) {
-    arr[i] = Number(prompt("enter element"))
-    sum = sum + arr[i];
+// let size = Number(prompt("enter size"))
+// let arr = new Array(size)
+// let sum = 0;
+// for(let i =0; i< arr.length; i++) {
+//     arr[i] = Number(prompt("enter element"))
+//     sum = sum + arr[i];
+// }
+
+// console.log("sum =" + sum)
+
+// find max number in an array
+let arr = [10,5,20,8,30]
+let maxNum = arr[0];
+for(let i = 1; i < arr.length; i++) {
+    if(arr[i] > maxNum) {
+        maxNum = arr[i]
+    }
 }
 
-console.log("sum =" + sum)
+console.log(maxNum);
