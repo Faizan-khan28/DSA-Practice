a1d9f271c764c92910b22af5413d8c61cc37423f
faizanKhan <faizankhan46849@gmail.com>
2026-07-31 07:35:49 +0530

    dis ques

--- revision/app.js
+++ revision/app.js
@@ -1,13 +1,19 @@
-let a = Number(prompt("enter a"))
-let b = Number(prompt("enter b"))
-let c = Number(prompt("enter c"))
-
-if(a>b && a<c) {
-    console.log("a is gretest" + a);
-}else if (b>c && b>a) {
-    console.log("b is gretest" + b);
+let amount = Number(prompt("enter amount"))
+let payableAmount = null
+if(amount >= 5000 && amount < 7000) {
+    let dis = (amount * 5) / 100
+    console.log("you get 5% Discount");
+    payableAmount = amount - dis
+}else if(amount >= 7000 && amount < 9000) {
+    let dis = (amount * 10) / 100
+    console.log("you get 10% Discount");
+    payableAmount = amount - dis
+}else if (amount >= 9000) {
+    let dis = (amount * 20) / 100
+    console.log("you get 20% Discount");
+    payableAmount = amount - dis
 }else {
-   console.log("c is gretest" + c);
+    console.log("you get 0% Discount")
 }
 
-console.log(a , "" ,b, "", c);
+console.log(payableAmount);
