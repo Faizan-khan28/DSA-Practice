7fe5babf80694ca58b5d92cae4b93bca8ba47536
faizanKhan <faizankhan46849@gmail.com>
2026-08-16 13:26:29 +0530

    array rotate by k element

--- revision/app.js
+++ revision/app.js
@@ -135,11 +135,23 @@ const promt = require("prompt-sync")();
 
 // console.log(arr)
 
+// let arr = [10,20,30,40,50]
+// let temp = arr[0]
+// for (let i = 0; i < arr.length-1; i++) {
+//     arr[i] = arr[i + 1]
+// }
+// arr[arr.length-1] = temp
+
+// console.log(arr.length-1)
+
 let arr = [10,20,30,40,50]
-let temp = arr[0]
-for (let i = 0; i < arr.length-1; i++) {
-    arr[i] = arr[i + 1]
+let k = 2;
+for (let j = 1; j <= k; j++) {
+    let temp = arr[0]
+    for (let i = 0; i < arr.length-1; i++) {
+     arr[i] = arr[i + 1] 
+    } 
+    arr[arr.length-1] = temp 
 }
-arr[arr.length-1] = temp
 
 console.log(arr)
