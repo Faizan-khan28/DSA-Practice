d473f08a13babd10ef1451bfbe6507938dcd67e1
faizanKhan <faizankhan46849@gmail.com>
2026-08-22 14:36:48 +0530

    binery Search

--- revision/app.js
+++ revision/app.js
@@ -144,14 +144,62 @@ const promt = require("prompt-sync")();
 
 // console.log(arr.length-1)
 
-let arr = [10,20,30,40,50]
-let k = 2;
-for (let j = 1; j <= k; j++) {
-    let temp = arr[0]
-    for (let i = 0; i < arr.length-1; i++) {
-     arr[i] = arr[i + 1] 
-    } 
-    arr[arr.length-1] = temp 
+// let arr = [10,20,30,40,50]
+// let temp = new Array(arr.length)
+// let k = 2;
+
+// for (let i = 0; i < arr.length; i++) {
+//      temp[i] = arr[(i+k)%arr.length]
+// }
+
+
+
+
+
+// for (let j = 1; j <= k; j++) {
+//     let temp = arr[0]
+//     for (let i = 0; i < arr.length-1; i++) {
+//      arr[i] = arr[i + 1] 
+//     } 
+//     arr[arr.length-1] = temp 
+// }
+
+// console.log(arr)
+
+// let arr = [12,30,40,50,35,60,75]
+// let index = -1;
+// let target = 75;
+// for (let i = 0; i <= arr.length; i++) {
+//     if (arr[i] == target) {
+//         index = i
+//         break
+//     }else {
+//         index
+//     }
+// }
+
+// console.log(index)
+
+let arr = [10,45,67,69,23,190,200]
+
+function BinarySearch (arr,target) {
+   let s = 0
+   let e = arr.length-1;
+   while(s<=e) {
+    let mid = (s+e)/2
+    if(arr[mid] == target) {
+        return mid
+    }else if(arr[mid] > target) {
+        e = mid -1
+    }else {
+        s = mid + 1
+    }
+   }
+   return -1
 }
 
-console.log(arr)
+if(BinarySearch(arr,69) === -1) {
+    console.log("Element not Found")
+}else {
+    console.log("Element Found")
+}
